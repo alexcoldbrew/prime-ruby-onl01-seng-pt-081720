c755a9f3af393c8a852209481ab72dad4924b194
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-25 20:57:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(integer)
+  # all primes are >= 2, so start there
   start = 2
-		 
+	
+	# accounting for negatives
 	if integer > 1
 		 
+		# creating a range, turning it into an array
 	  range = (start..integer-1).to_a
 		range.none? do |num|
 		integer % num == 0
